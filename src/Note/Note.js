c3a55176a02326600eea3d7acf7f676f3275828a
nevelson <nevelson92@gmail.com>
2020-05-14 16:28:14 -0400

    Added stuff

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -24,9 +24,10 @@ export default class Note extends React.Component {
       },
     })
       .then(res => {
-        if (!res.ok)
+        if (!res.ok){
           return res.json().then(e => Promise.reject(e))
-        return res.json()
+        }
+        return;
       })
       .then(() => {
         this.context.deleteNote(noteId)
